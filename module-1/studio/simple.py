39322bde92ca847c8022086f508927d20cc8d663
Vinicius Ribas <viniciusgribas@gmail.com>
2025-07-15 09:24:59 -0300

    m210

--- module-1/studio/simple.py
+++ module-1/studio/simple.py
@@ -1,39 +1,43 @@
-import random 
+import random
 from typing import Literal
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
+
 
 # State
 class State(TypedDict):
     graph_state: str
 
+
 # Conditional edge
 def decide_mood(state) -> Literal["node_2", "node_3"]:
-    
     # Often, we will use state to decide on the next node to visit
-    user_input = state['graph_state'] 
-    
+    user_input = state["graph_state"]
+
     # Here, let's just do a 50 / 50 split between nodes 2, 3
     if random.random() < 0.5:
-
         # 50% of the time, we return Node 2
         return "node_2"
-    
+
     # 50% of the time, we return Node 3
     return "node_3"
+
 
 # Nodes
 def node_1(state):
     print("---Node 1---")
-    return {"graph_state":state['graph_state'] +" I am"}
+    return {"graph_state": state["graph_state"] + " EU ESTOU"}
+
 
 def node_2(state):
     print("---Node 2---")
-    return {"graph_state":state['graph_state'] +" happy!"}
+    return {"graph_state": state["graph_state"] + " FELIZ!"}
+
 
 def node_3(state):
     print("---Node 3---")
-    return {"graph_state":state['graph_state'] +" sad!"}
+    return {"graph_state": state["graph_state"] + " TRISTE!"}
+
 
 # Build graph
 builder = StateGraph(State)
